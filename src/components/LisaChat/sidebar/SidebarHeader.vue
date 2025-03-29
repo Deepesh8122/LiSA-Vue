caf07d86f0f272994ffe2910a5664f397f52b867
UI Developer <ui@ha1ltd.co.uk>
2025-03-29 15:53:10 +0530

    fixed the npm issues

--- src/components/LisaChat/sidebar/SidebarHeader.vue
+++ src/components/LisaChat/sidebar/SidebarHeader.vue
@@ -7,7 +7,6 @@
       class="p-2 mt-[5px] rounded-lg transition-colors h-8 w-8 flex items-center justify-center cursor-pointer bg-neutral-200 hover:bg-neutral-300"
       aria-label="Hide sidebar"
       @click="$emit('toggle-sidebar')"
-       v-tooltip="'Enter your new'" type="text" placeholder="Right"
     >
       <MaterialIcon 
         name="side_navigation" 
@@ -20,7 +19,6 @@
 
 <script setup lang="ts">
 import MaterialIcon from '@/components/Icons/MaterialIcon.vue';
-import Tooltip from 'primevue/tooltip';
 
 defineEmits(['toggle-sidebar'])
 </script>
