d373ffd0cfc9fc97f465e75f359489834b71e8e8
aditya <aditya@gmail.com>
2025-07-15 10:21:01 +0000

    new changes

--- src/components/LisaChat/chat/MessageList.vue
+++ src/components/LisaChat/chat/MessageList.vue
@@ -58,11 +58,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, defineExpose } from 'vue'
+import { ref, watch } from 'vue'
 import MaterialIcon from '@/components/icons/MaterialIcon.vue'
-import LiSAIcon from '@/components/Icons/LiSAIcon.vue'
-import FileIcon from '@/components/Icons/FileIcon.vue'
-import DownloadIcon from '@/components/Icons/DownloadIcon.vue'
+import LiSAIcon from '@/components/icons/LiSAIcon.vue'
+import FileIcon from '@/components/icons/FileIcon.vue'
 import MarkdownRenderer from '@/components/LisaChat/chat/MarkdownRenderer.vue'
 
 interface Message {
